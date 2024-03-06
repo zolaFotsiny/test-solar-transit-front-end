0b4bb0f7e26eccaa400a94be18ecbe0d1d74034e
zolaFotsiny <zolaandriamarosoa@gmail.com>
2024-03-06 13:37:04 +0100

    Service dept, retrieve dept

--- src/components/departement/departementList.js
+++ src/components/departement/departementList.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import DepartementAdd from './departementAdd'
+import { getDept } from '../../services/serviceDept'
 
 export default function DepartementList() {
+
+
+    useEffect(() => {
+
+    }, []);
+
+    useEffect(() => {
+        getDept().then(rep => {
+            console.log('repppp', rep.data);
+        }).catch(err => {
+            console.log('somme err', err);
+        }).finally(() => {
+        });
+
+    }, [])
+
     return (
         // <div>departementList</div>
-        <DepartementAdd></DepartementAdd>
+        <div>
+            list
+            <DepartementAdd></DepartementAdd>
+
+        </div>
     )
 }
---
+++ src/services/serviceDept.js
@@ -0,0 +1,52 @@
+import axios from 'axios';
+
+
+
+const BASE_URL = "http://localhost:8080";
+
+
+const axiosInstance = axios.create({
+    baseURL: BASE_URL,
+    headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*', // Assurez-vous d'ajuster ceci en fonction de vos besoins
+    },
+});
+
+
+export const getDept = async () => {
+    try {
+
+        let response = '';
+        response = await axiosInstance.get(`${BASE_URL}/department`);
+        return response;
+    } catch (error) {
+        console.error('HUHU_ERROR lors de l\'envoi des données à l\'API :', error);
+        throw error;
+    }
+};
+
+export const saveDept = async (data) => {
+    try {
+        let response = '';
+        response = await axiosInstance.post(`${BASE_URL}/department`, data);
+        return response;
+    } catch (error) {
+        console.error('HUHU_ERROR lors de l\'envoi des données à l\'API :', error);
+        throw error;
+    }
+};
+
+export const updateDept = async (id, data) => {
+    try {
+        let response = '';
+        response = await axiosInstance.patch(`${BASE_URL}/department/${id}`, data);
+        return response;
+    } catch (error) {
+        console.error('HUHU_ERROR lors de l\'envoi des données à l\'API :', error);
+        throw error;
+    }
+};
+
+
+
